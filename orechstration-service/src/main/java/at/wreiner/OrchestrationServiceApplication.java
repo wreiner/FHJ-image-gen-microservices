1b7f5a8b794ac6bde3aacf349dc04d496e8e478b
Walter Reiner <walter.reiner@wreiner.at>
2024-04-25 11:13:46 +0200

    Remove unnecessary exception

--- orechstration-service/src/main/java/at/wreiner/OrchestrationServiceApplication.java
+++ orechstration-service/src/main/java/at/wreiner/OrchestrationServiceApplication.java
@@ -44,7 +44,7 @@ public class OrchestrationServiceApplication {
         return container;
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         SpringApplication.run(OrchestrationServiceApplication.class, args);
     }
 }
