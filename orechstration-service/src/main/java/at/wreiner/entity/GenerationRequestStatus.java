873f9f608c513e9949aa5909b84ea54efb93d5f5
Walter Reiner <walter.reiner@wreiner.at>
2024-04-26 10:01:37 +0200

    Add NSFW status

--- orechstration-service/src/main/java/at/wreiner/entity/GenerationRequestStatus.java
+++ orechstration-service/src/main/java/at/wreiner/entity/GenerationRequestStatus.java
@@ -1,5 +1,5 @@
 package at.wreiner.entity;
 
 public enum GenerationRequestStatus {
-    NEW, CLASSIFY, CLASSIFIED, GENERATE, DONE, FAILED
+    NEW, CLASSIFY, CLASSIFIED, GENERATE, DONE, FAILED, NSFW
 }
