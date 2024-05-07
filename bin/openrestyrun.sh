920b0a82daa8eddf2da1c26cf4633862675f1d1f
Walter Reiner <walter.reiner@wreiner.at>
2024-05-07 13:57:21 +0200

    Move openresty run script for dev

--- bin/openrestyrun.sh
+++ bin/openrestyrun.sh
@@ -2,5 +2,6 @@ docker run --rm --name openresty \
   -p 80:80 \
   -v /tmp/openresty/images:/fsimages \
   -v `pwd`:/etc/nginx/conf.d \
-  restyluahttp:0.1
+  192.168.2.102:5000/image-gen-cdn-openresty:0.1
+
 
